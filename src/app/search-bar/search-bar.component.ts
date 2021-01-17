acead413ae7f5eb47897360db602881654bcff26
Grama Ioan-Emanuel <gramai.emanuel@gmail.com>
2021-01-17 17:04:05 +0200

    WIP FE

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+
+export interface Criteria {
+  name: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  noCriteriaExist = false;
+  // criteria: Criteria[];
+
+  criteria: Criteria[] = [
+    {
+      name: 'Area',
+      value: 'Software Engineering'
+    },
+    {
+      name: 'Type',
+      value: 'IDE'
+    },
+    {
+      name: 'Programming Language',
+      value: 'Java'
+    }
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
